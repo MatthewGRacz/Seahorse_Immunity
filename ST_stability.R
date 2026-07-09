1f30e4e54ff94266b4fbd633d1ee7dd4e269a52e
Matthew G Racz <racz.matthewg@gmail.com>
2026-07-09 17:28:27 -0400

    Finished stability comparisons for the same number of supertypes! Next, I'll see if I can develop one for different supertype numbers, using similar methods

--- ST_stability.R
+++ ST_stability.R
@@ -1,98 +1,124 @@
-pca_base <- prcomp(zscores, center = TRUE, scale. = TRUE)
+library("clue")
 
-get_dapc_analysis <- function(zscores, num_tests, pipeline_path){
-  
-  #supertype and cluster used interchangeably here
-  
-  #report other summary stats of the cluster estimates
+setwd("/Users/mattracz/Projects/Wilson_Lab")
+
+get_dapc_analysis <- function(zscores, num_supertypes, pipeline_path){
   
   cluster_data <- find.clusters(zscores, 
-                                n.clust=17,
+                                n.clust=num_supertypes,
                                 n.pca=ncol(zscores))
-  
-  #shows BIC graph of clusters, to find optimal number of clusters
-  #keep n.pca=ncol(zscores) because you want to use all the variance possible, to overshoot
-  #the mode cluster number is used as the number of clusters
   
   test_dapc <- dapc(zscores, 
                     cluster_data$grp, 
                     n.pca=ncol(zscores), 
                     n.da=1000)
-  
-  #all PCs kept from find.clusters for all amino acids' Z-scores
-  #overshooting helps use all values between 1 and the overshot npca number
-  #helps to find the most conservative, statistically probable npca number
-  #n.da maxes out at n.pca-1, finds the maximum number of axes for the cluster comparisons; 
-  #setting n.da too high will cause it to self-correct to the maximum possible value
-  
-  a_spline_data <- optim.a.score(test_dapc, n.sim=10)
-  #finds the optimal number of principal components
+    
+  a_spline_data <- optim.a.score(test_dapc, n.sim=1)
   
   final_dapc <- dapc(zscores, 
                      grp = cluster_data$grp, 
                      n.pca = a_spline_data$best,
                      n.da = 1000)
   
-  #the groups are the actual separated groups using the best data, accounting for every minor detail
-  #DAPC can now get the best analysis of the recombinants using only the optimized number of PCs
-  #it looks at the top n.pca separations and keeps those, while the rest are deleted
-  
-  cat("\n", final_dapc$var * 100, "% of Variance explained using ", final_dapc$n.pca, " PCs and ", length(final_dapc$eig), "/", length(final_dapc$pca.eig), " eigenvalues")
-  #how much variance is captured by the optimal number of PCs, 
-  #and how many eigenvalues were possible
-  
   indv_by_supertype_df <- data.frame(Recombinant = row.names(zscores),
                                      Supertype = final_dapc$assign)
   
-  #which cluster is each recombinant in
-  
-
-  
-  #this gets all of the linear discriminants and plots them against each other in a multi-page plot 
-  #helps visualize X-dimensional space, where X is the number of LDs
-  
-  return(list(final_dapc, a_spline_data))
+  return(final_dapc)
   
 }
+#gets a DAPC for each run, where it assigns supertypes to the recombinants
 
-num_runs <- 3
+num_runs <- 5
 
-results <- replicate(num_runs, get_dapc_analysis(zscores, 1, "Pipeline/"), simplify = FALSE)
+num_supertypes <- 17
 
-View(results)
+cat("\nRunning DAPC analyses... This may take a moment...")
 
-dapcs <- lapply(1:num_runs, function(i) {
+results <- replicate(num_runs, get_dapc_analysis(zscores, num_supertypes, "Pipeline/"), simplify = FALSE)
+#run num_supertypes many DAPC analyses
+
+merged_dapc <- data.frame(
   
-  dapc_obj <- results[[i]][[1]] 
+  RECOMBINANT = rownames(zscores),
   
-  df <- data.frame(
-    Recombinant = rownames(zscores),
-    Assignment = dapc_obj$assign
-  )
+  setNames(lapply(results, function(x){ x$assign }), paste0("RUN_", 1:num_runs))
   
-  colnames(df)[2] <- paste0("Run_", i)
+)
+#all of the DAPC supertypes for each recombinant in a massive dataframe
+
+ari_df2 <- data.frame(
   
-  return(df)
+  RUN = paste0("RUN_", 2:num_runs),
+  
+  ARI = sapply(2:num_runs, function(i) { 
+    
+    adjustedRandIndex(merged_dapc[["RUN_1"]], merged_dapc[[paste0("RUN_", i)]]) 
+    
+  })
+)
+#puts into a dataframe the ARI for each run's DAPC compared to run1's 
+
+comparison_cols <- colnames(merged_dapc)[colnames(merged_dapc) != c("RECOMBINANT", "RUN_1")]
+#the columns that run1's supertype numbers are compared to, to form the supertype dictionary
+
+translated_cols <- lapply(comparison_cols, function(col_x){
+  
+  translated_supertypes <- as.numeric(solve_LSAP(table(merged_dapc[["RUN_1"]], merged_dapc[[col_x]]), maximum = TRUE))
+  #compares run1 to runX and puts them into a cross-matrix
+  #feeds that cross-matrix to the Hungarian Algorithm
+  
+  supertype_dictionary <- setNames(1:max(as.numeric(levels(merged_dapc[[col_x]]))), translated_supertypes)
+  #creates a dictionary based on the Hungarian Algorithm's best supertype numbers match
+  
+  return(as.numeric(supertype_dictionary[as.character(merged_dapc[[col_x]])]))
+  #translates the supertype numbers from runX into run1-equivalent supertypes
+  #returns the column of runX's supertypes as run1-translated supertype numbers
+  
 })
 
-merged_dapc <- Reduce(function(x, y) merge(x, y, by = "Recombinant", suffixes = c("", "_new")), dapcs)
-merged_dapc <<- merged_dapc
+jumper_df <- data.frame(RECOMBINANT=merged_dapc[["RECOMBINANT"]], 
+                        INDIVIDUAL=sub("_.*", "", merged_dapc[["RECOMBINANT"]]), 
+                        RUN_1=merged_dapc[["RUN_1"]], 
+                        setNames(translated_cols, toupper(comparison_cols)))
 
-View(merged_dapc)
+#a bit hardcoded, but for a better look
+#assigns names and makes runX have runX's translated supertypes
 
-ari_scores <- sapply(2:num_runs, function(i) { adjustedRandIndex(merged_dapc[["Run_1"]], merged_dapc[[paste0("Run_", i)]]) })
+stabilities <- lapply(jumper_df$RECOMBINANT, function(recomb){
+  
+  recomb_data <- as.numeric(jumper_df[jumper_df$RECOMBINANT==recomb, -c(1, 2)])
+  
+  best_supertype <- as.numeric(names(which.max(table(recomb_data))))
+  
+  return((sum(recomb_data == best_supertype)-1) * 100 / (length(recomb_data)-1))
+  
+})
 
-names(ari_scores) <- paste0("Run_", 2:num_runs)
+jumper_df$STABILITY <- as.numeric(unlist(stabilities))
+#adds up, out of the total number of runs, the number of runs with the same, most frequent supertype number
+#the minus 1 excludes run1 comparing with itself 
+#this is the optimistic approach, so if run1 was an outlier, and a recombinant is mostly in another supertype, 
+#that supertype is deemed the best-match and therefore the percent of the time that the recombinant is within 
+#the better supertype is counted instead
 
-cat("\n--- True Adjusted Rand Index (ARI) Scores ---\n")
+recomb_stabilities <- data.frame(jumper_df["RECOMBINANT"], jumper_df["INDIVIDUAL"], jumper_df["STABILITY"])
+#summary stats dataframe, with each recombinant, its individual and its stability, next to each other 
 
-print(ari_scores)
-cat("\nMean: ", mean(ari_scores))
-cat("\nMedian: ", median(ari_scores))
+indv_stabilities <- aggregate(STABILITY ~ INDIVIDUAL, data = jumper_df, FUN = function(x) {
+  c(MEDIAN = median(x, na.rm = TRUE),
+    MEAN   = mean(x, na.rm = TRUE),
+    RANGE  = diff(range(x, na.rm = TRUE)))
+})
 
+indv_stabilities <- cbind(indv_stabilities["INDIVIDUAL"], indv_stabilities$STABILITY)
+#gets the median, mean, and range stabilities per individual, for individual-based analyses 
 
-table(merged_dapc$Run_1, merged_dapc$Run_2)
+write_csv(jumper_df, paste0(pipeline_path, "jumpers_dataframe.csv"))
 
+write_csv(recomb_stabilities, paste0(pipeline_path, "recombinant_stabilities.csv"))
 
+write_csv(indv_stabilities, paste0(pipeline_path, "individual_stabilities.csv"))
+
+write_csv(ari_df2, paste0(pipeline_path, "ari_df2.csv"))
 
 
